45a0b3165cf494358fe3a46e0d092ec18420350a
Joshua Bertucci <joshua.bertucci@deakin.edu.au>
2024-11-29 23:18:04 +1100

    Finished task 3.2P

---
+++ 3.2P/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+//* CONSTANTS
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, function () {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
